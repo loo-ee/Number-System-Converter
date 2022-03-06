5c67e00541f3bd82f339996c5cf1abede81ba445
CoderLui <92241727+lui523202@users.noreply.github.com>
2022-03-06 22:42:58 +0800

    Stable build of the project :)

--- COE/test.cpp
+++ COE/test.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
+#include "numberSystemClass.h"
 using namespace std;
 
-int main ()
-{
-    cout << "Hello world" << endl;
+int main () {
+    long long int val;
+    NumberSystem newClass;
+
+    cout << "Enter a long long int: ";
+    cin >> val;
+
+    newClass.setIntResult(val);
+    cout << "Value: " << newClass.getIntResult() << endl;
 }
